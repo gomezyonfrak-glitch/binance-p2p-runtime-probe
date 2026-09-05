c1d1471b00646e4fdd5272fde6f62d65f9bd40d9
gomezyonfrak-glitch <gomezyonfrak@gmail.com>
2026-09-04 23:45:34 -0300

    Test Binance web inventory rows 50

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ const publicSellPix = new URL(publicSell);
 publicSellPix.searchParams.set("tradeMethodIdentifiers", "Pix");
   const webPayload = (tradeType, pix = false) => ({
   page: 1,
-  rows: 20,
+  rows: 50,
   asset: "USDT",
   fiat: "BRL",
   tradeType,
